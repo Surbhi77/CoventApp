7bbeb1923a4d70794de3b71b78b6e48fa4b18fc7
Surbhi <dev.vitechso@gmail.com>
2020-12-13 16:38:44 +0530

    changes in environment file

--- src/app/innovator-listing/innovator-listing.component.ts
+++ src/app/innovator-listing/innovator-listing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router,ActivatedRoute} from '@angular/router';
 import {ApiService} from './../services/api.service';
+import {environment} from 'environments/environment'
 @Component({
   selector: 'ngx-innovator-listing',
   templateUrl: './innovator-listing.component.html',
@@ -9,9 +10,10 @@ import {ApiService} from './../services/api.service';
 export class InnovatorListingComponent implements OnInit {
   categoryId: any;
   innovatorListing:any=[];
-  imageBasePath:any='http://134.209.68.96:9700/'
+  imageBasePath:any=environment.imageUrl;
   getSubCategory: any=[];
   searchString:any='';
+  subCategory:any=0;
 
   constructor(private router:Router,
     private apiService:ApiService, 
@@ -25,6 +27,38 @@ export class InnovatorListingComponent implements OnInit {
 
       let body = document.getElementsByTagName('body')[0];
       body.classList.add("absolute-header");
+    }
+
+    onImgError($event){
+      $event.target.src = './assets/images/placeholder.jpg'
+    }
+
+    modelChanged($event){
+      console.log($event);
+      let obj={
+        "keywords":this.searchString,
+        "subcat_id":0
+      };
+      if(this.subCategory>0){
+        obj.subcat_id=this.subCategory
+      }
+      this.apiService.searchInnovatorListing(obj).subscribe(res=>{
+        this.innovatorListing = res['data']
+      })
+    }
+
+    searchChanged(){
+      console.log(this.searchString)
+      let obj={
+               "keywords":"",
+               "subcat_id":this.subCategory
+              };
+      if(this.searchString!=''){
+        obj.keywords = this.searchString
+      }
+      this.apiService.searchInnovatorListing(obj).subscribe(res=>{
+        this.innovatorListing = res['data']
+      })
     }
 
     getSubcategoryListing() {
